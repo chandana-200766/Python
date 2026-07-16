e2b0a14f70c5f95db50b4c2d0afcb735954c563d
chandana-200766 <chandanamarripati6@gmail.com>
2026-07-16 20:43:57 +0530

    Create Practice.py

---
+++ Practice.py
@@ -0,0 +1,9 @@
+import sys
+int=66
+char='A'
+float=2.3
+double=33.567
+print("size of char=",sys.getsizeof(char),"bytes")
+print("size of int=",sys.getsizeof(int),"bytes")
+print("size of float=",sys.getsizeof(float),"bytes")
+print("size of double=",sys.getsizeof(double),"bytes")
