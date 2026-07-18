93af1a431949e639883d35e43798eaaf342fbc16
chandana-200766 <chandanamarripati6@gmail.com>
2026-07-18 19:14:01 +0530

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,23 @@
+a=5
+b=6
+sum=a+b
+subtract=a-b
+multiply=a*b
+divide=a/b
+c= a % b
+d= a ** b
+e= a // b
+print("Sum:", sum)
+print("Subtract:", subtract)
+print("Multiply:", multiply)
+print("Divide:", divide)
+print("Modulus:", c)
+print("Exponent:", d)
+print("Floor Division:", e)
+n=int(input("enetr a number:"))
+m=int(input("enetr a number:"))
+if n==m:
+    print("Both numbers are equal")
+else:
+    print("Both numbers are not equal")
+
